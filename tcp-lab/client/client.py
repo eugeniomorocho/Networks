fca3e00b0b9c692286dbbf118bf282abb78d225e
eugeniomorocho <sf4e@outlook.com>
2025-09-02 08:56:58 -0500

    Update client

--- tcp-lab/client/client.py
+++ tcp-lab/client/client.py
@@ -6,6 +6,6 @@ PORT = 12345
 
 s = socket.socket()
 s.connect((HOST, PORT))
-data = s.recv(1024)
+data = s.recv(1024) # Maximum number of bytes to receive per reading block
 print("Received:", data.decode())
 s.close()
