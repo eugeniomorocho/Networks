179e707ec775c44cf7f7b06b4b795991141f9033
eugeniomorocho <sf4e@outlook.com>
2025-09-02 05:14:04 -0500

    Added server and client in tcp-lab

---
+++ tcp-lab/client/client.py
@@ -0,0 +1,11 @@
+# client.py
+import socket
+
+HOST = '<EC2-public-IP>'
+PORT = 12345
+
+s = socket.socket()
+s.connect((HOST, PORT))
+data = s.recv(1024)
+print("Received:", data.decode())
+s.close()
