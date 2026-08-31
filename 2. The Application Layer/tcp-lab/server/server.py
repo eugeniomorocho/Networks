ec574b504d34643216cd22783ea7c787dcbbeacb
eugeniomorocho <sf4e@outlook.com>
2026-08-31 14:49:45 -0500

    Added comments to server.py

--- 2. The Application Layer/tcp-lab/server/server.py
+++ 2. The Application Layer/tcp-lab/server/server.py
@@ -5,9 +5,9 @@ PORT = 12345 # Defines the TCP port where the server will listen
 
 s = socket.socket() # Create a TCP socket. Default arguments: socket.AF_INET (IPv4), socket.SOCK_STREAM (TCP)
 s.bind((HOST, PORT)) # Bind the socket to the specified address and port
-s.listen(1) # Start listening for incoming connections (1 pecifies the backlog, i.e. how many pending connections the OS can queue while the application is not yet accepting them)
+s.listen(1) # Start listening for incoming connections (1 pecifies the backlog, i.e. how many pending connections the OS can queue while the application is not yet accepting them). Default backlog is usually 5, but here we set it to 1 for simplicity. If the backlog is exceeded, new connection attempts may be refused or ignored by the OS.
 print("TCP server waiting for connection...") # Print that the server is ready to accept connections
 conn, addr = s.accept() # Accept an incoming connection. This method blocks until a connection is established. It returns a new socket object specifically for connecting with that client (conn), and the client's network address and port (addr)
 print(f"Connected to {addr}") # Print the address of the connected client (addr is a tuple containing the client's IP address and port number)
-conn.send(b"Hello Eugenio, from the EC2 server!") # Send a message to the connected client. The message must be in bytes, hence the 'b' prefix before the string
+conn.send("Hello Eugenio, from the EC2 server!".encode()) # Send a message to the connected client. The message must be in bytes, hence the use of the encode() in a UTF-8 encoding method to convert the string into a bytes object. The send() method transmits the data over the established TCP connection. If the client is not ready to receive data, this method may block until the client is ready or until a timeout occurs.
 conn.close() # Close the connection with the client. This is important to free up system resources and avoid potential issues with too many open connections
